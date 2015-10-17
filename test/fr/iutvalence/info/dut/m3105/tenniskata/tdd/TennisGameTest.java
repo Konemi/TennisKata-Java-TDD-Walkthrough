b833a713d1f3e03a53d4038a5515a04e700b24af
sebastienjean <baz.jean@gmail.com>
2015-10-17 18:00:44 +0200

    Iteration 2: Refactor
    - extracted TennisGame constructor call in TennisGameTest test methods
    as a "Before" method (i.e. automatically called before each test)
    initializing a field called tennisGame

--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -1,23 +1,30 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
-
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest
 {
+	private TennisGame tennisGame ;
+	
+	@Before
+	public void newTennisGame()
+	{
+		this.tennisGame = new TennisGame();
+	}
+	
 	@Test
 	public void ifServerHasNotScoredAndReceiverHasNotScoredScoreShouldBe_love_all()
 	{
-		TennisGame tennisGame = new TennisGame();
-		assertEquals(tennisGame.getScore(), "love-all");
+		assertEquals(this.tennisGame.getScore(), "love-all");
 	}
 	
 	@Test
 	public void ifServerHasScored1TimeAndReceiverHasNotScoredScoreShouldBe_fifteen_love()
 	{
-		TennisGame tennisGame = new TennisGame();
-		tennisGame.serverHasScored();
-		assertEquals(tennisGame.getScore(), "fifteen-love");
+		this.tennisGame.serverHasScored();
+		assertEquals(this.tennisGame.getScore(), "fifteen-love");
 	}
 }
