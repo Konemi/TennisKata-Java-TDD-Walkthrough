320ffce4eff47f9fe9bddd6965dc8d82734de26b
sebastienjean <baz.jean@gmail.com>
2015-10-18 09:00:37 +0200

    Iteration 4: Red
    - added tests for scores that are not tied above 2/2 points scored
    (tests added by adding values to TennisGameScoreExpectation enum)

--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameScoreExpectation.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameScoreExpectation.java
@@ -3,8 +3,17 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public enum TennisGameScoreExpectation
 {
 	LOVE_ALL(0,0,"love-all"),
+	LOVE_FIFTEEN(0,1,"love-fifteen"),
+	LOVE_THIRTY(0,2,"love-thirty"),
+	LOVE_FORTY(0,3,"love-forty"),
 	FIFTEEN_LOVE(1,0,"fifteen-love"),
-	LOVE_FIFTEEN(0,1,"love-fifteen");
+	FIFTEEN_ALL(1,1,"fifteen-all"),
+	FIFTEEN_THIRTY(1,2,"fifteen-thirty"),
+	FIFTEEN_FORTY(1,3,"fifteen-forty"),
+	THIRTY_LOVE(2,0,"thirty-love"),
+	THIRTY_FIFTEEN(2,1,"thirty-fifteen"),
+	THIRTY_ALL(2,2,"thirty-all"),
+	THIRTY_FORTY(2,3,"thirty-forty");
 
 	private final int serverPoints;
 	
