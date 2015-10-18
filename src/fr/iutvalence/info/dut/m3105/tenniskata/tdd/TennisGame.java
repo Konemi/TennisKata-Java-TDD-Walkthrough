5fe5fd6e8358330473cb740705bce3c919f46e4a
sebastienjean <baz.jean@gmail.com>
2015-10-18 09:21:13 +0200

    Iteration 6: Refactor
    - extracted constants in TennisGame class

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -2,6 +2,10 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
 public class TennisGame
 {
+	private static final int POINT_AMOUNT_FOR_ADVANTAGE_SCORE = 4;
+
+	private static final int POINT_AMOUNT_FOR_DEUCE_SCORE = 3;
+
 	private final static String[] INDIVIDUAL_SCORE = new String[] {"love", "fifteen", "thirty", "forty"};
 	
 	private int serverPoints;
@@ -29,7 +33,7 @@ public class TennisGame
 	
 	private String getScoreWhenTied()
 	{
-		if (this.serverPoints == 3)
+		if (this.serverPoints == POINT_AMOUNT_FOR_DEUCE_SCORE)
 			return "deuce";
 		else
 			return INDIVIDUAL_SCORE[this.serverPoints]+"-all";
@@ -37,9 +41,9 @@ public class TennisGame
 
 	private String getScoreWhenNotTied()
 	{
-		if (this.serverPoints == 4)
+		if (this.serverPoints == POINT_AMOUNT_FOR_ADVANTAGE_SCORE)
 			return "advantage in";
-		if (this.receiverPoints == 4)
+		if (this.receiverPoints == POINT_AMOUNT_FOR_ADVANTAGE_SCORE)
 			return "advantage out";
 		
 		return INDIVIDUAL_SCORE[this.serverPoints]+"-"+INDIVIDUAL_SCORE[this.receiverPoints];
